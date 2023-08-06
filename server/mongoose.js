caf6f4d2347d0f20781a2d05f41dfb94bec55db5
kaden-ator <kkadenfugate@gmail.com>
2023-08-05 22:26:04 -0700

    small update
    simplified structure slighly, added join group functionality

---
+++ server/mongoose.js
@@ -0,0 +1,124 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.createConnection('mongodb+srv://' +
+    process.env.USERNAME +
+    ':' +
+    process.env.PASSWORD +
+    '@connectifydb.tjeylyr.mongodb.net/connectify_db?retryWrites=true&w=majority'
+);
+
+// create user mongoose schema
+const userSchema = new mongoose.Schema({                                        
+
+    email: String,
+    username: String,
+    lowercase_username: String,
+    password: String,
+    access_key: String,
+    groups: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Group"
+    }]
+
+});     
+
+const User = mongoose.model('User', userSchema);
+
+// create user schema in mongoose
+function create_user(email, username, password, access_key){
+    
+    return new User({
+        email:   email.toLowerCase(),
+        username:   username,
+        lowercase_username: username.toLowerCase(),
+        password:   password,
+        access_key: access_key,
+        groups: []
+    });
+
+}
+
+// add user to DB
+async function add_user(user){
+
+    // .save adds user to db
+    try{ user.save(); }
+    catch(err){ console.log(err.message); }
+
+}
+
+// check that email is already in DB (not case sensitive)
+async function email_exists(email){
+
+    // will return true when findOne does not return null
+    if( await User.findOne({ email: email }) != null ){ return true; } 
+    else{ return false; }
+
+}
+
+// check if username in DB (not case sensitive)
+async function username_exists(username){
+
+    if( await User.findOne({ lowercase_username: username }) != null ){ return true; }
+    else{ return false; }
+
+}
+
+// clear db [ REMOVE UPON COMPLETION ]
+async function clear_db(){ for( user of await User.find({}) ){ await User.deleteOne({ _id: user._id }); } }
+
+
+/**************************************************************************************************************/
+/*                                           GROUP SCHEMA FUNCTIONS                                           */
+/**************************************************************************************************************/
+
+// hold group as an object containing the name, port, owner, and members of group
+const groupSchema = new mongoose.Schema({
+
+    group_name: String,
+    spotify_port: String,
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+async function create_group(name, owner_id){
+
+    const owner = await User.findById(owner_id);
+
+    return new Group({
+        group_name: name,
+        spotify_port: owner.access_key,
+        owner: owner_id,
+        members: [owner_id]
+    });
+}
+
+async function add_group(group){
+
+    try{ await group.save(); }
+    catch(err){ console.error(err); }
+
+}
+
+async function join_group(user_id, group_id){
+
+    // add user to list of members in group
+    await Group.update(
+        { _id: group_id },
+        { $push: { members: user_id } }
+    );
+
+}
+
+module.exports = { create_user, add_user, email_exists, username_exists, clear_db, create_group, add_group, join_group }
