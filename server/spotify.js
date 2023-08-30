09b646b38f2c5c8905b4d5e2b5ed426711712df1
kaden-fugate <kkadenfugate@gmail.com>
2023-08-30 10:38:24 -0700

    Updated user permission scope
    Added several permissions to get from user. This is to implement all desired features for the app such as allowing owner of a group to add to queue, pause/resume music, change music, show entire group the past played songs, currently playing song, etc.

--- server/spotify.js
+++ server/spotify.js
@@ -6,12 +6,11 @@ var REDIRECT_URI = 'http://localhost:3000/create_account';
 
 function makeAuthURL()
 {
-
     return AUTHORIZATION_URL + querystring.stringify({
         client_id:  process.env.CLIENT_ID,
         response_type:  'code',
         redirect_uri:   encodeURI(REDIRECT_URI),
-        scope: 'user-read-private'
+        scope: 'user-read-private user-read-email user-library-read user-read-playback-state user-modify-playback-state user-read-playback-state '
     });
 }
 
