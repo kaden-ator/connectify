e9f6eee55609b5028e36c711be8ab1175127d6f8
kaden-fugate <kkadenfugate@gmail.com>
2023-08-30 13:02:17 -0700

    Added sliding feature for group page
    Added a feature that allows a screen to slide onto the page from underneath the footer, pushing the footer up alongside it. The page that is pushed up from pressing the button will hold a users top recommended songs. They can use these songs or search songs to suggest songs to add to queue.

--- public/scripts/group.js
+++ public/scripts/group.js
@@ -4,6 +4,25 @@ window.addEventListener('DOMContentLoaded', async () => {
     const group = get_group();
     const user = JSON.parse( localStorage.getItem('user') );
 
+    const queueIcon = document.querySelector(".queue-icon");
+    const hiddenPage = document.querySelector(".hidden-page");
+    const footer = document.querySelector(".footer");
+
+    queueIcon.addEventListener("click", function () {
+        console.log(hiddenPage.style.display);
+        // Toggle the position of the hidden page and the height of the footer
+        if (hiddenPage.style.display === '') {
+            hiddenPage.style.top = '40px';
+            footer.style.bottom = 'calc(100vh - 40px)';
+            hiddenPage.style.display = 'block';
+        } 
+        else{
+            hiddenPage.style.top = '100vh';
+            footer.style.bottom = '0';
+            hiddenPage.style.display = '';
+        }
+    });
+
 });
 
 // function will return the id of the current group
